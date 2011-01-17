092b5ae20cc4229291a7ef1174f570582443c473
Craig Russell <craig@craig-russell.co.uk>
2011-01-17 18:13:00 +0000

    Bootstrapper deconstructs SEO friendly urls

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,9 +1,38 @@
 <?php
 
-/* Get SEO friendly url */
-$url = $_GET['url'];
 
-echo $url;
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
+
+function callHook(){
+    
+    /*  Break apart SEO friendly urls 
+        galleon.com/class/function/param1/param2    */
+    if (isset($_GET['url'])){
+        $urlClean       = preg_replace('/[^\/a-z0-9]/','',strtolower(trim($_GET['url'],'/')));
+        $urlParams      = explode("/", $urlClean);
+        $urlClass       = array_shift($urlParams);
+        $urlFunction    = array_shift($urlParams);
+    }else{
+        /* Defaults */
+        $urlParams      = array();
+        $urlClass       = '';
+        $urlFunction    = '';
+    }
+    
+    $galleonClass = ucwords($urlClass).'Controller';
+    
+    echo "<pre>";
+    echo "Class: ".$urlClass."\n";
+    echo "Galleon Class: ".$galleonClass."\n";
+    echo "Function: ".$urlFunction."\n";
+    print_r($urlParams);
+    echo "</pre>";
+}
+
+/*
+    Autoload classes as needed
+*/
+function __autoload($className){
+
+}
+
+callHook();
