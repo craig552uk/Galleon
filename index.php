dc273ca88c63704043abdce88bfec158208f0aa1
Craig Russell <craig@craig-russell.co.uk>
2011-01-18 15:52:51 +0000

    disabled developer debugging

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
 
 /* Enable / disable development messages*/
-define('DEVELOPMENT_ENVIRONMENT', true);
+define('DEVELOPMENT_ENVIRONMENT', false);
 
 /* Lets get on with it! */
 include_once (ROOT . DS . 'lib' . DS . 'Galleon.Bootstrap.php');
