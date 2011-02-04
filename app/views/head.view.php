814eef04dfd4bef07522f4b8c919f30ecfedaa5c
Craig Russell <craig@craig-russell.co.uk>
2011-02-04 11:12:45 +0000

    Loaded icons, css and js in head view with resource library functions and head.js

--- app/views/head.view.php
+++ app/views/head.view.php
@@ -1,19 +1,40 @@
-<?php global $config; /* Get config data in scope */?>
-
+<?php global $config; /* Get config data within scope */?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8" >
-       
+<head>    
+    <!-- Document meta data -->
     <title><?php echo $title; ?></title>
+    <meta charset="utf-8" >
     <meta name="author" content="<?php echo $config['meta']['author']; ?>">
     <meta name="description" content="<?php echo $config['meta']['description']; ?>">
     
+    <!-- Force nice browser behaviours -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="shortcut icon" href="<?php echo $config['galleon']['root_path']; ?>/favicon.ico">
-    <link rel="apple-touch-icon" href="<?php echo $config['galleon']['root_path']; ?>/apple-touch-icon.png">
-  
+    <!-- Icon images -->
+    <link rel="shortcut icon" href="<?php ga_res_img('favicon'); ?>">
+    <link rel="apple-touch-icon" href="<?php ga_res_img('apple-touch-icon'); ?>">
+    
+    <!-- Style sheets -->
+    <link rel="stylesheet" href="<?php ga_res_css('meyer-reset-2-0b2'); ?>">
+    <link rel="stylesheet" href="<?php ga_res_css('desktop-styles'); ?>">
+    <link rel="stylesheet" href="<?php ga_res_css('ipad-styles'); ?>">
+    <link rel="stylesheet" href="<?php ga_res_css('smartphone-styles'); ?>">
+    
+    <!-- Configure and load head.js http://headjs.com -->
+    <script>var head_conf = { screens: [640, 1024, 1280, 1680] };</script>
+    <script src="<?php ga_res_js('head'); ?>"></script>
+    
+    <script>
+        /* Load scripts in parallel, execute in order */
+        head.js('<?php ga_res_js("jquery-1.4.4"); ?>');
+        head.js('<?php ga_res_js("jquery-ui-1.8.9.custom"); ?>');
+        
+        /* Wait for scripts to load then... */
+        head.ready(function() {
+           
+        });
+    </script>
 </head>
 <body>
