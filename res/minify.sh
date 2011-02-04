e80925585c614bff47f19cf304e549ebf48c4827
Craig Russell <craig@craig-russell.co.uk>
2011-02-04 15:45:28 +0000

    Added YUI compressor for minification. Wrote bulk minify script

---
+++ res/minify.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+#   Bulk minify script
+#   Will minify all css and js scripts without .min.css or .min.js extensions 
+#   Will create a minified copy of the file in the appropriate folder
+#
+#   NOTE: Be sure to run script from script folder
+#         ./minify.sh
+#
+#   Author: Craig Russell
+#   Email:  craig@craig-russell.co.uk
+#
+
+CSS="css"
+JS="js"
+FILELIST="minify.$$.tmp"
+
+COMPORESSOR="yuicompressor-2.4.2.jar"
+
+ls $CSS | grep -v min > $FILELIST
+while read LINE
+do
+    OLD="$CSS/$LINE"
+    NEW=` echo "$CSS/$LINE" | sed 's/.css/.min.css/g'`
+    
+    echo "$OLD -> $NEW"
+    java -jar yuicompressor-2.4.2.jar $OLD > $NEW
+    
+done < $FILELIST
+
+ls $JS | grep -v min > $FILELIST
+while read LINE
+do
+    OLD="$JS/$LINE"
+    NEW=` echo "$JS/$LINE" | sed 's/.js/.min.js/g'`
+    
+    echo "$OLD -> $NEW"
+    java -jar yuicompressor-2.4.2.jar $OLD > $NEW
+    
+done < $FILELIST
+
+rm -f $FILELIST
