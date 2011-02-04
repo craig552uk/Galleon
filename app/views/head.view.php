7ac49f311d13b48e16fd69b16b809b7105a4576e
Craig Russell <craig@craig-russell.co.uk>
2011-02-04 10:10:54 +0000

    Added HTMl meta to provent mobile scalling, force latest IE rendering, for favicon and apple touch icon images

--- app/views/head.view.php
+++ app/views/head.view.php
@@ -4,10 +4,16 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" >
-    
+       
     <title><?php echo $title; ?></title>
     <meta name="author" content="<?php echo $config['meta']['author']; ?>">
     <meta name="description" content="<?php echo $config['meta']['description']; ?>">
     
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="shortcut icon" href="<?php echo $config['galleon']['root_path']; ?>/favicon.ico">
+    <link rel="apple-touch-icon" href="<?php echo $config['galleon']['root_path']; ?>/apple-touch-icon.png">
+  
 </head>
 <body>
