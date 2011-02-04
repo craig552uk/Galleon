0689c378a731d5589e26eb3968f7f5aa4d96169d
Craig Russell <craig@craig-russell.co.uk>
2011-02-04 16:57:43 +0000

    Added validation links to tail view

--- app/views/tail.view.php
+++ app/views/tail.view.php
@@ -1,2 +1,7 @@
+<section id="validation">
+    Validate
+    <a href="http://validator.w3.org/check?uri=<?php echo $config['galleon']['domain'].$_SERVER['REQUEST_URI']; ?>">HTML</a>
+    <a href="http://jigsaw.w3.org/css-validator/validator?uri=<?php ga_res_css('styles', true); ?>">CSS</a>
+</section>
 </body>
 </html>
